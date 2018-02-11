0c1e57d55a6a3fee1b9903b8f34ff00d009a36f2
Rasmus Tibell <rasmus.tibell@gmail.com>
2018-02-11 21:08:35 +0000

    Fixed LCD display presentation

--- python/src/DisplaySensor.py
+++ python/src/DisplaySensor.py
@@ -1,27 +1,46 @@
+#!/usr/bin/python
+from RPLCD import CharLCD
 import RPi.GPIO as GPIO
 import time
 import sys
 
-LED = 16
-TRIG = 20
-ECHO = 21
-SENS = 22
+#BCD
+#----
+LED = 36 #16
+TRIG = 38 #20
+ECHO = 40 #21
+SENS = 15 #22
 GPIO.setwarnings(False)
-#GPIO.setmode(GPIO.BOARD)
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED, GPIO.OUT)
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 GPIO.setup(SENS, GPIO.IN)
 
+#BOARD
+#------
+LCD_RW = 21
+LCD_RS = 37
+LCD_E = 35
+LCD_D0 = 23
+LCD_D1 = 29
+LCD_D2 = 31
+LCD_D3 = 33
+lcd = CharLCD(cols=16, rows=2, pin_rw=LCD_RW, pin_rs=LCD_RS, pin_e=LCD_E, pins_data=[LCD_D3, LCD_D2, LCD_D1, LCD_D0], numbering_mode=GPIO.BOARD)
+dsp_line1 = ""
+dsp_line2 = ""
 
 def main():
+	lcd.clear()
+	lcd.cursor_pos = (0, 0)
 	GPIO.output(TRIG,False)
 	time.sleep(2)
-	simple()
+	#simple()
 	measure_time()
 
 def measure_time():
+	GPIO.output(LED,False)
 	for i in range(1,20):
 		time.sleep(0.5)
 		GPIO.output(TRIG,True)
@@ -40,14 +59,19 @@ def measure_time():
 		print delta
 		print speed
 		print "----------"
+		if speed > 1.0:
+			GPIO.output(LED,True)
+		else:
+			GPIO.output(LED,False)
+		dsp("Distance: %1.2f" % speed, "Sensor: %d" % GPIO.input(SENS))
 
 
 def simple():
 	isHI = False
 	isLO = False
+	GPIO.output(LED,True)
 	for i in range(1,100):
-		GPIO.output(LED,True)
-		time.sleep(0.05)
+		time.sleep(0.1)
 		sens1 = GPIO.input(ECHO)
 		if sens1 == True:
 			isHI = True
@@ -60,7 +84,15 @@ def simple():
 		time.sleep(0.0001)
 		sens3 = GPIO.input(ECHO)
 		print "values " + str(sens1) + " " + str(sens2) + " " + str(sens3) +  " |  " + str(GPIO.input(SENS)) + " |  " + str(isHI) + " " + str(isLO)
-		GPIO.output(LED,False)
+
+	GPIO.output(LED,False)
+
+def dsp(line1, line2):
+	lcd.cursor_pos = (0, 0)
+	lcd.write_string(line1)
+	lcd.cursor_pos = (1, 0)
+	lcd.write_string(line2)
+
 
 if __name__ == "__main__":
     main()
